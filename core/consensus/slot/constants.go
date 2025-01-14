efc2e57321d175785e0ab60dece095ceb86623a5
Emerson Barros <emersonfb99@gmail.com>
2025-01-14 15:54:12 -0300

    [KLC-873] Fixes time discrepancy between DoWork() and processReceivedBlock() (#912)
    When executing [`DoWork()`](https://github.com/klever-io/klever-go/blob/a1457bdd18fb38dc10536e281c2904c242d94bf7/core/consensus/slot/subslot.go#L118C1-L151C2), which handles the `startSlot()`, `endSlot()`, `slotBlock()`, and `slotSignature()` jobs performed by the leader and validators within a block, the maximum execution time is set to 80% ([`MaxThresholdPercent`](https://github.com/klever-io/klever-go/blob/a1457bdd18fb38dc10536e281c2904c242d94bf7/core/consensus/slot/constants.go#L6C1-L6C34)) of the block duration. In Klever's blockchain, where each block lasts 4 seconds, this equates to 3.2 seconds. If `DoWork()` exceeds this limit, the [`Extend()`](https://github.com/klever-io/klever-go/blob/a1457bdd18fb38dc10536e281c2904c242d94bf7/core/consensus/slot/subslot.go#L142C1-L148C16) function is triggered, causing the block to be reverted.
    
    However, when the leader sends the block and header to validators for processing via the [`processReceivedBlock()`](https://github.com/klever-io/klever-go/blob/a1457bdd18fb38dc10536e281c2904c242d94bf7/core/consensus/slot/bls/subslotBlock.go#L280C1-L348C2) function, a maximum processing time of 85% ([`processingThresholdPercent`](https://github.com/klever-io/klever-go/blob/a1457bdd18fb38dc10536e281c2904c242d94bf7/core/consensus/slot/bls/constants.go#L41C1-L41C38)), [defined on the instantiation of `subslotBlock`](https://github.com/klever-io/klever-go/blob/a1457bdd18fb38dc10536e281c2904c242d94bf7/core/consensus/slot/bls/blsSubslotsFactory.go#L193C1-L201C1), of the block duration is allowed—5% more than `DoWork()`. This discrepancy means that if a block is large or complex and requires the full 85% to process, `DoWork()` will prematurely cancel what would otherwise be a valid block after 80% of the block duration.
    
    Increase `DoWork()` maximum execution time to 90% of the block duration, ensuring alignment with `processReceivedBlock()` and avoiding unnecessary block cancellations.

--- core/consensus/slot/constants.go
+++ core/consensus/slot/constants.go
@@ -3,7 +3,7 @@ package slot
 //TODO consider moving these constants in config file
 
 // MaxThresholdPercent specifies the max allocated time percent for doing Job as a percentage of the total time of one slot
-const MaxThresholdPercent = 80 //
+const MaxThresholdPercent = 90 //
 
 // LeaderPeerHonestyIncreaseFactor specifies the factor with which the honesty of the leader should be increased
 // if it proposed a block or sent the final info, in its correct allocated slot/time-frame/slot
