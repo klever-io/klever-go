94cf77b2c4dae75231d7016e8c100f9e3ce3ecc8
Fernando Sobreira <fbsobreira@gmail.com>
2025-11-03 15:31:27 -0500

    facade(node): add node facade unit tests, export helpers and mocks

---
+++ common/mock/apiResolverStub.go
@@ -0,0 +1,53 @@
+package mock
+
+import (
+	"github.com/klever-io/klever-go/core"
+	"github.com/klever-io/klever-go/core/process"
+	"github.com/klever-io/klever-go/data/transaction"
+	"github.com/klever-io/klever-go/vmcommon"
+)
+
+// APIResolverStub -
+type APIResolverStub struct {
+	EstimateTransactionGasCalled func(tx *transaction.Transaction) (*transaction.CostResponse, error)
+	ExecuteSCQueryCalled          func(query *process.SCQuery) (*vmcommon.VMOutput, error)
+	StatusMetricsCalled           func() core.StatusMetricsHandler
+	GetTotalStakedValueCalled     func() (int64, error)
+}
+
+// EstimateTransactionGas -
+func (a *APIResolverStub) EstimateTransactionGas(tx *transaction.Transaction) (*transaction.CostResponse, error) {
+	if a.EstimateTransactionGasCalled != nil {
+		return a.EstimateTransactionGasCalled(tx)
+	}
+	return nil, nil
+}
+
+// ExecuteSCQuery -
+func (a *APIResolverStub) ExecuteSCQuery(query *process.SCQuery) (*vmcommon.VMOutput, error) {
+	if a.ExecuteSCQueryCalled != nil {
+		return a.ExecuteSCQueryCalled(query)
+	}
+	return nil, nil
+}
+
+// StatusMetrics -
+func (a *APIResolverStub) StatusMetrics() core.StatusMetricsHandler {
+	if a.StatusMetricsCalled != nil {
+		return a.StatusMetricsCalled()
+	}
+	return nil
+}
+
+// GetTotalStakedValue -
+func (a *APIResolverStub) GetTotalStakedValue() (int64, error) {
+	if a.GetTotalStakedValueCalled != nil {
+		return a.GetTotalStakedValueCalled()
+	}
+	return 0, nil
+}
+
+// IsInterfaceNil -
+func (a *APIResolverStub) IsInterfaceNil() bool {
+	return a == nil
+}
---
+++ common/mock/nodeHandlerStub.go
@@ -0,0 +1,86 @@
+package mock
+
+import (
+	"encoding/json"
+
+	"github.com/klever-io/klever-go/config"
+	"github.com/klever-io/klever-go/core"
+	kdafeespool "github.com/klever-io/klever-go/core/kapp/kdaFeesPool"
+	"github.com/klever-io/klever-go/data/api"
+	"github.com/klever-io/klever-go/data/state"
+	"github.com/klever-io/klever-go/data/transaction"
+	indexerData "github.com/klever-io/klever-go/indexer/data"
+	"github.com/klever-io/klever-go/kapps"
+	heartbeatData "github.com/klever-io/klever-go/node/heartbeat/data"
+	"github.com/klever-io/klever-go/tools/debug"
+)
+
+// NodeHandlerStub - minimal stub for NodeHandler interface
+type NodeHandlerStub struct{}
+
+func (n *NodeHandlerStub) StartConsensus() error                          { return nil }
+func (n *NodeHandlerStub) ValidateTransaction(*transaction.Transaction, bool) error {
+	return nil
+}
+func (n *NodeHandlerStub) SendTransaction(*transaction.Transaction) (string, error) { return "", nil }
+func (n *NodeHandlerStub) SendBulkTransactions([]*transaction.Transaction) ([]string, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) CreateTransaction(uint32, *transaction.TXBaseInfo, []json.RawMessage, bool) (*transaction.Transaction, []byte, error) {
+	return nil, nil, nil
+}
+func (n *NodeHandlerStub) DecodeTransaction(*transaction.Transaction) (*indexerData.Transaction, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) GetTransaction(string, bool) (*api.Transaction, error) { return nil, nil }
+func (n *NodeHandlerStub) EstimateTransactionFees(*transaction.Transaction) (*transaction.FeesResponse, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) TXPool(string, int, int) ([]*api.Transaction, int, error) {
+	return nil, 0, nil
+}
+func (n *NodeHandlerStub) GetAccount(string) (state.UserAccountHandler, error) { return nil, nil }
+func (n *NodeHandlerStub) GetNextNonce(string) (uint64, uint64, uint64, error) {
+	return 0, 0, 0, nil
+}
+func (n *NodeHandlerStub) GetBalance(string, string) (int64, error) { return 0, nil }
+func (n *NodeHandlerStub) GetUserKDA(string, string) (*kapps.UserKDA, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) GetAvailableClaim(string, string) (int64, map[string]int64, int64, error) {
+	return 0, nil, 0, nil
+}
+func (n *NodeHandlerStub) GetAsset(string) (*kapps.KDAData, error) { return nil, nil }
+func (n *NodeHandlerStub) GetNFT(string, string) (*kapps.UserKDA, *kapps.KDAData, error) {
+	return nil, nil, nil
+}
+func (n *NodeHandlerStub) GetKDAFeePool(string) (*kdafeespool.KDAFeesPoolData, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) GetMarketplace(string) (*api.Marketplace, error) { return nil, nil }
+func (n *NodeHandlerStub) GetHeartbeats() []heartbeatData.PubKeyHeartbeat  { return nil }
+func (n *NodeHandlerStub) IsInterfaceNil() bool                            { return n == nil }
+func (n *NodeHandlerStub) ValidatorStatisticsAPI() (map[string]*state.ValidatorApiResponse, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) PeersAPI() ([]state.PeerAccountHandler, error) { return nil, nil }
+func (n *NodeHandlerStub) EncodeAddressPubkey([]byte) (string, error)    { return "", nil }
+func (n *NodeHandlerStub) DecodeAddressPubkey(string) ([]byte, error)    { return nil, nil }
+func (n *NodeHandlerStub) GetQueryHandler(string) (debug.QueryHandler, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) GetPeerInfo(string) ([]core.QueryP2PPeerInfo, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) GetProposalParameters() (map[int32]*kapps.Parameter, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) GetBlockByHash(string, bool) (*api.Block, error) { return nil, nil }
+func (n *NodeHandlerStub) GetBlockByNonce(uint64, bool) (*api.Block, error) {
+	return nil, nil
+}
+func (n *NodeHandlerStub) SetRedundancy(int64) error               { return nil }
+func (n *NodeHandlerStub) GetRedundancy() int64                    { return 0 }
+func (n *NodeHandlerStub) GetEnableEpochs() (config.EnableEpochsConfig, error) {
+	return config.EnableEpochsConfig{}, nil
+}
